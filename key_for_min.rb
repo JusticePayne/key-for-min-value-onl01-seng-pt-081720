1c62d58bc20fead49e8586439a142d79257e556b
Justice Payne <spikepayne@yahoo.com>
2020-08-30 18:12:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,10 @@
 def key_for_min_value(name_hash)
-  return nil if name_hash.size==0
-  min_key.min_value_name_hash.first
-    if value<min_value
-    min_key=key
-    end
+   return nil if name_hash.size == 0
+   min_key, min_value = name_hash.first
+   name_hash.each do |key, value|
+      if value < min_value
+      min_key = key
+      end
+   end
+   min_key
 end
