c10b9e5c31c7f5ad8ba4bee72a48d61ef38b3dac
Justice Payne <spikepayne@yahoo.com>
2020-08-30 18:09:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
-
+  return nil if name_hash.size==0
+  min_key.min_value_name_hash.first
+    if value<min_value
+    min_key=key
+    end
+  end
+  min_key
 end
+
+min_key
